7db4c3326525385ecd7644ca21afb9b2621fdcf1
wangledongle <2640716535@qq.com>
2018-09-24 00:08:09 +0800

    fix bug

--- HomeWork1/E1.py
+++ HomeWork1/E1.py
@@ -4,28 +4,37 @@ import string
 import numpy as np
 import time
 import nltk
-import numpy as np
 from nltk.corpus import stopwords
 PATH = 'C:\\Users\\wangl\\Documents\\20news-18828'
+PPath = 'C:\\Users\\wangl\\Documents\\dou\\'
+ppppath='C:\\Users\\wangl\\Documents\\'
 stoplist=stopwords.words('english')
 diffwords = set()
-dirlist = []
-filelist=[]
 # 获得一个文件夹下的所有子文件夹的绝对路径
-def dir_list():
-    for root,dirname,filename in os.walk(PATH):
+def dir_list(path):
+    dirlist=[]
+    for root,dirname,filename in os.walk(path):
         for name in dirname:
             dirlist.append(os.path.join(root,name))
-
+    return dirlist
 # 获得一个文件夹下的所有文件的绝对路径
-def file_list():
+def file_list(dirlist):
+    filelist=[]
     for dirname in dirlist:
         for root,dirs,files in os.walk(dirname):
             for subfile in files:
                 filelist.append(os.path.join(root,subfile))
-
-def splitword():
+    return filelist
+    
+def splitword(filelist):
+        wlist = set()
+        filelist = [filename for filename in filelist if '.txt' not in filename]
+        print(len(filelist))
+        d=1
         for filename in filelist:
+            print(d)
+            d=d+1
+            gf = open(PPath + filename.split('\\')[-2]+filename.split('\\')[-1]+'.txt','w')
             f = open(filename, 'r', errors='ignore')
             lines=f.readlines()
             for line in lines:
@@ -34,51 +43,35 @@ def splitword():
                     for words in sentence:
                         lowword=words.lower()
                         if lowword.isalpha():
-                            if lowword not in stoplist and lowword not in diffwords:
-                                diffwords.add(lowword)
+                            if lowword not in stoplist and len(lowword)>1:
+                                #if lowword not in diffwords:
+                                    diffwords.add(lowword)
+                                #if lowword not in wlist:
+                                    wlist.add(lowword)
                         else:
                             if lowword.find('@')==-1:
                                 text = re.sub(r"[^a-zA-Z0-9]", ' ', lowword).split()
                                 text =[t for t in text if not any(ch.isdigit() for ch in t) ]
                                 for it in text:
-                                    if len(it)>1 and it not in stoplist and it not in diffwords:
+                                    if len(it) > 1 and it not in stoplist :
+                                            #if it not in diffwords:
                                         diffwords.add(it)
+                                            #if it not in wlist:
+                                        wlist.add(it)
+            for i in wlist:
+                gf.write(i + '\n')
+            wlist.clear()
             f.close()
-
-def countword():
-    diffwoodlist=list(diffwords)
-    rows = len(filelist)
-    cols = len(diffwords)
-    arr=np.zeros((rows,cols),dtype=np.int8)
-    for row in range(0, rows):
-        f = open(filelist[row], 'r', errors='ignore')
-        lines = f.readlines()
-        for line in lines:
-            if line is not '\n':
-                sentence = line.split()
-                for words in sentence:
-                    lowword=words.lower()
-                    if lowword.isalpha() :
-                        if lowword not in stoplist:
-                            #arr[row][diffwoodlist.index(lowword)]=arr[row][diffwoodlist.index(lowword)]+1
-                            arr[row][diffwoodlist.index(lowword)]=1
-                    else :
-                        if lowword.find('@')==-1:
-                            text=re.sub(r"[^a-zA-Z0-9]",' ',lowword).split()
-                            text=[t for t in text if not any(ch.isdigit() for ch in t)]
-                            for it in text:
-                                if len(it) > 1 and it not in stoplist:
-                                    #arr[row][diffwoodlist.index(it)]=arr[row][diffwoodlist.index(it)]+1
-
-                                    arr[row][diffwoodlist.index(it)]=1
-        f.close()
-    print("begin save")
-    np.savetxt('C:\\Users\\wangl\\Documents\\result.txt',arr)
+            gf.close()
+        gf=open(ppppath+'\\data.txt','w')
+        for i in diffwords:
+            gf.write(i + '\n')
+        gf.close()
 def main():
-    dir_list()
-    file_list()
-    splitword()
-    countword()
+    dirlist=dir_list(PATH)
+    filelist=file_list(dirlist)
+    splitword(filelist)
+    #countword()
 if __name__ == "__main__":
     start=time.clock()
     main()
